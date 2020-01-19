0b9a19608aaff9457679079ede3e7e4058853a2b
Nurbek8848 <nurbeksoltoev@gmail.com>
2020-01-19 18:42:39 +0600

    Java Doc. Cod convention.[#220678]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,22 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculator Class for calculating arithmetic operations + - * /.
+ * @author Nurbek
+ * @since 19.01.2020
+ * @version 1
+ */
 public class Calculator {
+    /**
+     * Constructor, output of the results of arithmetic operations.
+     * @param args - arg.
+     */
+    public static void main(String[] args) {
+        add(1, 1);
+        div(4, 2);
+        multiple(2, 1);
+        subtrack(10, 5);
+    }
     public static void add(double first, double second) {
         double result =  first + second;
         System.out.println(first + "+" + second + " = " + result);
@@ -17,10 +33,4 @@ public class Calculator {
         double result =  first - second;
         System.out.println(first + "-" + second + " = " + result);
     }
-    public static void main(String[] args) {
-        add(1, 1);
-        div(4, 2);
-        multiple(2, 1);
-        subtrack(10, 5);
-    }
-}
+ }
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+package ru.job4j.calculator;
+/**
+ * Package for calculate task.
+ *
+ * @author Nurbek Soltoev (nurbeksoltoev@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
