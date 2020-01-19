cb5d006385aa3918c0856dc4d03fe6a0e9c321ba
Nurbek8848 <nurbeksoltoev@gmail.com>
2020-01-19 16:48:44 +0600

    Ипотека.[#220672]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+        double credit = amount + (amount * (percent / 100));
+         while(credit > 0) {
+            credit -= salary;
+            year++;
+            credit += credit * (percent / 100);
+           }
+        return year;
+    }
+}
