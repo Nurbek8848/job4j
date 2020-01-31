dc30e9616e7bf02f4ffe02a5bec8bfca4a6dfaef
Nurbek8848 <nurbeksoltoev@gmail.com>
2020-01-31 23:18:34 +0600

    Рефакторинг - Площадь треугольника.[#220762]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,11 +1,47 @@
 package ru.job4j.condition;
 
 public class Triangle {
-    public static boolean exist(double ab, double ac, double bc) {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+    public boolean exist(double a, double b, double c) {
         boolean result = false;
-        if (ab + ac > bc && ac + bc > ab && bc + ab > ac) {
+        a = first.distance(second);
+        b = first.distance(third);
+        c = second.distance(third);
+        if (a + b > c && b + c > a && c + a > b) {
             result = true;
         }
         return result;
+    }
+    public double perimeter(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return p;
+    }
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = perimeter(a, b, c);
+        if (this.exist(a, b, c)) {
+            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return rsl;
+    }
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 1);
+        Point c = new Point(1, 1);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        System.out.println(result);
+
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -7,19 +8,14 @@ import static org.junit.Assert.assertThat;
 public class TriangleTest {
 
     @Test
-    public void triangleExist() {
-        double inAB = 1.5;
-        double inAC = 2.5;
-        double inBC = 3.5;
-        boolean result = Triangle.exist(inAB, inAC, inBC);
-        assertThat(result, is(true));
-    }
-    @Test
-    public void triangleNotExist() {
-        double inAB = 1.5;
-        double inAC = 2.5;
-        double inBC = 6.5;
-        boolean result = Triangle.exist(inAB, inAC, inBC);
-        assertThat(result, is(false));
+   public void area() {
+        Point a = new Point(1, 1);
+        Point b = new Point(1, 3);
+        Point c = new Point(3, 3);
+        Triangle triangle = new Triangle(a, b, c);
+        double expected = 2;
+        double result = triangle.area();
+        Assert.assertEquals(expected, result, 0.1);
+
     }
 }
