31dbcaa7bec059d2f74099c92424616a729f1696
Nurbek8848 <nurbeksoltoev@gmail.com>
2020-02-02 14:08:10 +0600

    Конструктор по умолчанию.[#220776]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Animal.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+public class Animal {
+
+    public Animal() {
+        System.out.println("Class Animal");
+    }
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+    }
+
+}
---
+++ chapter_002/src/main/java/ru/job4j/tracker/Predator.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+public class Predator extends Animal {
+
+    public Predator() {
+        System.out.println("Class Predator");
+    }
+    public static void main(String[] args) {
+        Predator predator = new Predator();
+    }
+}
