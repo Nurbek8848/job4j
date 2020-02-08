f1de9256d734a8c22999a2c6f1e73bf2ae0bb8a8
Nurbek8848 <nurbeksoltoev@gmail.com>
2020-02-08 14:18:41 +0600

    Реализовать класс Tracker[#220787]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,57 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Tracker {
+
+    private final Item[] items = new Item[100];
+    private int position = 0;
+
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        items[this.position++] = item;
+        return item;
+    }
+    public Item[] findAll() {
+        Item[] all = new Item[this.items.length];
+        int size = 0;
+        for (int i = 0; i < this.items.length; i++) {
+            Item item = this.items[i];
+            if (item != null) {
+                all[size] = item;
+                size++;
+            }
+        }
+         all = Arrays.copyOf(all, size);
+        return all;
+    }
+    public Item[] findByName(String key) {
+        Item[] byName = new Item[this.items.length];
+        int size = 0;
+        for (int i = 0; i < this.items.length; i++) {
+            Item item = this.items[i];
+            if (key.equals(item.getName())) {
+                byName[size] = item;
+                size++;
+            }
+        }
+        return Arrays.copyOf(byName, size);
+    }
+    public Item findById(String id) {
+        Item findId = new Item();
+        for (int i = 0; i < this.items.length; i++) {
+            Item item = this.items[i];
+            if (id.equals(item.getId())) {
+                findId = item;
+            } else {
+                findId = null;
+            }
+        }
+        return findId;
+    }
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
 }
